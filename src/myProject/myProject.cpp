cc1f11f751dc3df7ef754f6894f4ebd75f8d8c52
ladislas <ladislas@weareleka.com>
2014-07-01 09:32:35 +0200

    create simple code to test compilation

--- src/myProject/myProject.cpp
+++ src/myProject/myProject.cpp
@@ -1,77 +1,21 @@
-#define serial Serial
-
 #include <Arduino.h>
-#include <Wire.h>
+#include "MyFirstLib.h"
 
-#include "ChibiOS_AVR.h"
-#include "Serial.h"
-#include "Tools.h"
-
-#include "CommunicationUtils.h"
-#include "DebugUtils.h"
-#include "ADXL345.h"
-#include "ITG3200.h"
-#include "FreeIMU.h"
-
-#include "Sensors.h"
-#include "Led.h"
-#include "DriveSystem.h"
-#include "Motor.h"
-
-
-#include "./lib/Arbitrer.h"
-#include "./lib/Cruise.h"
-#include "./lib/Bump.h"
-#include "./lib/Stabilization.h"
-
-void chSetup() {
-	chThdSleepMilliseconds(5000);
-
-	chThdCreateStatic(waArbitrerThread, sizeof(waArbitrerThread),
-			NORMALPRIO + 10, ArbitrerThread, NULL);
-	chThdCreateStatic(waStabilizationThread, sizeof(waStabilizationThread),
-			NORMALPRIO, StabilizationThread, NULL);
-	chThdCreateStatic(waCruiseThread, sizeof(waCruiseThread),
-			NORMALPRIO + 1, CruiseThread, NULL);
-	chThdCreateStatic(waBumpThread, sizeof(waBumpThread),
-			NORMALPRIO + 2, BumpThread, NULL);
-
-	/* chThdSleepMilliseconds(6000);
-	   Serial.println("YOLO");
-	   robot.spin(sensors, RIGHT, 160, 90);
-
-	   chThdSleepMilliseconds(600);
-
-	   robot.go(FORTH, 130, 3000, 350);
-	   robot.stop(500);
-
-	//chThdSleepMilliseconds(1000);
-
-	robot.spin(sensors, LEFT, 160, 90);
-
-	chThdSleepMilliseconds(600);
-
-	robot.go(BACK, 130, 3000, 350);
-	robot.stop(500);
-
-	Serial.println("SWAG");
-	robot.stop();*/
-}
+MyFirstClass MyFirstObject;
 
 void setup() {
+
 	Serial.begin(115200);
-	sensors.init();
+	delay(1000);
 
-	Serial.println("111");
-
-	chBegin(chSetup);
-
-	while(1) {
-		// nothing to do here
-	}
 }
 
 void loop() {
-	// nothing to do here
+
+	MyFirstObject.methodNumberOne();
+	delay(1000);
+	MyFirstObject.methodNumberTwo();
+	delay(1000);
+
 }
 
