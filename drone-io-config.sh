04d932f43932ea2784a494d13dc8b7de88c69f88
ladislas <ladislas@weareleka.com>
2014-09-04 13:09:56 +0200

    test nex drone config

--- drone-io-config.sh
+++ drone-io-config.sh
@@ -1,8 +1,12 @@
-# Environnement variables - must be copied in Drone.io
-# CWD=/home/ubuntu/src/github.com/WeAreLeka/Bare-Arduino-Project
-# SRC=/home/ubuntu/src/github.com/WeAreLeka/Bare-Arduino-Project/src
-# TEST=/home/ubuntu/src/github.com/WeAreLeka/Bare-Arduino-Project/test
+# INSTRUCTIONS: copy and past the following inside your drone.io project setting.
 
+# Environnement variables - must be copied in the reserved space
+CWD=/home/ubuntu/src/github.com/WeAreLeka/Bare-Arduino-Project
+SRC=/home/ubuntu/src/github.com/WeAreLeka/Bare-Arduino-Project/src
+TEST=/home/ubuntu/src/github.com/WeAreLeka/Bare-Arduino-Project/test
+ARDMK=/home/ubuntu/src/github.com/WeAreLeka/Bare-Arduino-Project/Arduino-Makefile
+
+# Commands - must be copied in the reserved space
 echo "Install avr-gcc..."
 wget --quiet  http://downloads.arduino.cc/arduino-1.5.7-linux32.tgz
 tar -xf arduino-1.5.7-linux32.tgz
@@ -15,19 +19,16 @@ sudo mv arduino-1.0.5/ /usr/local/share/arduino
 
 echo "Cloning Arduino-Makefie and activate auto-lib..."
 git submodule init && git submodule update
-cd $CWD/Arduino-Makefile
+cd $ARDMK
 git checkout auto-lib
 
-echo "Test myFirstProject..."
-cd $SRC/myFirstProject
-git clone https://gist.github.com/ladislas/230f38359c3526fba944 ./tmp
-cp ./tmp/Makefile ./
-make
-
-echo "Test mySecondProject..."
-cd $SRC/mySecondProject
-git clone https://gist.github.com/ladislas/230f38359c3526fba944 ./tmp
-cp ./tmp/Makefile ./
-make
-
+echo "Test all sources..."
+cd $SRC
+for dir in *; do
+	if [ -d "${dir}" ]; then
+		cd $dir
+		cp $CWD/Makefile-Drone.mk Makefile
+		make
+	fi
+done
 
