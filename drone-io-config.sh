be130c7e8349a00fda6917927852966820719968
ladislas <ladislas@weareleka.com>
2014-09-04 12:13:37 +0200

    try new drone config

--- drone-io-config.sh
+++ drone-io-config.sh
@@ -1,10 +1,12 @@
+# Environnement variables - must be copied in Drone.io
+# CWD=/home/ubuntu/src/github.com/WeAreLeka/Bare-Arduino-Project
+# SRC=/home/ubuntu/src/github.com/WeAreLeka/Bare-Arduino-Project/src
+# TEST=/home/ubuntu/src/github.com/WeAreLeka/Bare-Arduino-Project/test
+
 echo "Install avr-gcc..."
 wget --quiet  http://downloads.arduino.cc/arduino-1.5.7-linux32.tgz
 tar -xf arduino-1.5.7-linux32.tgz
 sudo mv arduino-1.5.7/hardware/tools/avr /usr/local/share/avr-gcc
-
-# echo "Test avr-gcc..."
-# /usr/local/share/avr-gcc/bin/avr-gcc -v
 
 echo "Install Arduino IDE..."
 wget --quiet  http://arduino.googlecode.com/files/arduino-1.0.5-linux32.tgz
@@ -13,18 +15,17 @@ sudo mv arduino-1.0.5/ /usr/local/share/arduino
 
 echo "Cloning Arduino-Makefie and activate auto-lib..."
 git submodule init && git submodule update
-cd Arduino-Makefile
+cd $CWD/Arduino-Makefile
 git checkout auto-lib
-cd ..
 
 echo "Test myFirstProject..."
-cd src/myFirstProject
+cd $SRC/myFirstProject
 git clone https://gist.github.com/ladislas/230f38359c3526fba944 ./tmp
 cp ./tmp/Makefile ./
 make
 
 echo "Test mySecondProject..."
-cd ../mySecondProject
+cd $SRC/mySecondProject
 git clone https://gist.github.com/ladislas/230f38359c3526fba944 ./tmp
 cp ./tmp/Makefile ./
 make
