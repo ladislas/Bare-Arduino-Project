6b193588d6a5b4f2760a88094432cd0402d035ab
ladislas <ladislas@weareleka.com>
2014-09-16 15:06:31 +0200

    test avr-gcc

--- script/install.sh
+++ script/install.sh
@@ -12,14 +12,16 @@ AVR_GCC_URL="http://downloads.arduino.cc/$AVR_GCC_FILE"
 
 echo "Installing Arduino 1.0.5..."
 
-wget --quiet "$ARDUINO_URL"
+wget "$ARDUINO_URL"
 tar -xzf "$ARDUINO_FILE"
 sudo mv "$ARDUINO_BASENAME/" "/usr/local/share/arduino"
 
 echo "Installing avr-gcc from Arduino 1.5.7..."
 
-wget --quiet "$AVR_GCC_URL"
+wget "$AVR_GCC_URL"
 tar -xzf "$AVR_GCC_FILE"
 sudo mv "$AVR_GCC_BASENAME/hardware/tools/avr" "/usr/local/share/avr-gcc"
 
+echo "Installtion of dependencies is complete, we are now going to run some tests..."
 
+/usr/local/share/avr-gcc/avr-gcc -v
--- script/runtests.sh
+++ script/runtests.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-set -e
-
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
